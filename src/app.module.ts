04f777d5b94d6b829a66db724df75fd8edf46fa7
SJ0826 <ikosdu60@gmail.com>
2025-02-08 23:45:29 +0900

    feat: 예외 필터 적용

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,6 +13,9 @@ import {
   WinstonModule,
   utilities as nestWinstonModuleUtilities,
 } from 'nest-winston';
+import { APP_FILTER } from '@nestjs/core';
+import { HttpExceptionFilter } from './exeption/http-exception.filter';
+import { ExceptionModule } from './exeption/exception.module';
 
 @Module({
   imports: [
@@ -40,24 +43,9 @@ import {
       entities: [__dirname + '/**/*.entity{.ts,.js}'], // TypeORM이 구동될 때 인식하도록 할 엔티티 클래스 경로
       synchronize: process.env.DATABASE_SYNCHRONIZE === 'true', // dev | 서비스가 실행될 때 DB가 초기화된다.
     }),
-    /**
-     * logging config
-     */
-    // WinstonModule.forRoot({
-    //   transports: [
-    //     new winston.transports.Console({
-    //       level: process.env.NODE_ENV === 'production' ? 'info' : 'silly',
-    //       format: winston.format.combine(
-    //         winston.format.timestamp(),
-    //         nestWinstonModuleUtilities.format.nestLike('Myapp', {
-    //           prettyPrint: true,
-    //         }),
-    //       ),
-    //     }),
-    //   ],
-    // }),
     AuthModule,
     LoggerModule,
+    ExceptionModule,
   ],
   controllers: [],
   providers: [],
